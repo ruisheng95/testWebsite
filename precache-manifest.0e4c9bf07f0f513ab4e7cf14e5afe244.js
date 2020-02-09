7ed8ece191cad36f94e144dc6195b8b08ac1036e
ruisheng <ng@ruisheng.me>
2020-02-09 22:10:07 +0800

    Updates

---
+++ precache-manifest.0e4c9bf07f0f513ab4e7cf14e5afe244.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17920143cdd046a23f1c07dc7f6cb71d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d27eb6d6364ed0837248",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ef9804a867f77f9e15ac",
+    "url": "/static/js/2.ec1606be.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.ec1606be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d27eb6d6364ed0837248",
+    "url": "/static/js/main.67f7d123.chunk.js"
+  },
+  {
+    "revision": "6e7d85ad969107dd34f7",
+    "url": "/static/js/runtime-main.54edee9e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
